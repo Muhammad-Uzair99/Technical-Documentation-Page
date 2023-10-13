b0d85bad2062ac211590552c217fa85663f9cbdc
Muhammad-Uzair99 <uzairmaster97@gmail.com>
2023-10-13 17:55:56 +0500

    Update Tailwind CSS styles and HTML structure
    Explanation: This commit updates the Tailwind CSS configuration in the tailwind.config.js file, and refines the styles in the input.css file to ensure a more consistent and visually appealing design for the Technical Documentation Page. It also optimizes the HTML structure in the index.html file to maintain a responsive and user-friendly layout. These changes enhance the overall user experience and readability of the technical documentation.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'text': '#fdf7f7',
+        'background': '#0f0d0d',
+        'primary': '#70d2d2',
+        'secondary': '#0c0c27',
+        'accent': '#3dc2c2',
+       },       
+    },
   },
   plugins: [],
 }
